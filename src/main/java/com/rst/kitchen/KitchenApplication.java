cca0fe8e45b96793b1590f1197db0dc7a61d6db3
OleksandrFliagin <samme0062@gamil.com>
2023-11-22 21:22:46 +0200

    init

---
+++ src/main/java/com/rst/kitchen/KitchenApplication.java
@@ -0,0 +1,13 @@
+package com.rst.kitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenApplication.class, args);
+	}
+
+}
